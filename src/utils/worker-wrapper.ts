72515ae539533b03da298815e96daaa6e0cb0857
Yaroslav Serhieiev <noomorph@gmail.com>
2025-09-24 18:20:25 +0300

    fix: remove dangling legacy leftovers

--- src/utils/worker-wrapper.ts
+++ src/utils/worker-wrapper.ts
@@ -1,10 +1,6 @@
 export class WorkerWrapper {
   constructor(private readonly worker: any) {}
 
-  get artifactsManager() {
-    return this.worker._artifactsManager as ArtifactsManager;
-  }
-
   get asyncWebSocket() {
     return this.worker._client._asyncWebSocket as AsyncWebSocket;
   }
@@ -23,36 +19,10 @@ export class WorkerWrapper {
   }
 }
 
-interface ArtifactsManager {
-  _idlePromise: Promise<void>;
-  _artifactPlugins: {
-    log?: ArtifactPlugin;
-  };
-  on(event: string, callback: (...args: any[]) => void): void;
-}
-
 interface EventEmitter {
   on(event: 'beforeLaunchApp', callback: () => void): void;
   on(event: 'launchApp', callback: (event: { pid: number }) => void): void;
   on(event: 'terminateApp', callback: () => void): void;
-}
-
-interface ArtifactPlugin {
-  onBeforeCleanup: () => void;
-  onBeforeLaunchApp: () => void;
-  onBeforeShutdownDevice: () => void;
-  onBeforeTerminateApp: () => void;
-  onBootDevice: () => void;
-  onHookFailure: () => void;
-  onLaunchApp: () => void;
-  onRunDescribeFinish: () => void;
-  onRunDescribeStart: () => void;
-  onShutdownDevice: () => void;
-  onTerminate: () => void;
-  onTerminateApp: () => void;
-  onTestDone: () => void;
-  onTestFnFailure: () => void;
-  onTestStart: () => void;
 }
 
 interface AsyncWebSocket {
